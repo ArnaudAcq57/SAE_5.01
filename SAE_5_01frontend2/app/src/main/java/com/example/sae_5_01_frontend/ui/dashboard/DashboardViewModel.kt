d4b454bb173abf9a9b5829b82f4b78e0741e8453
iclidark <o.abbaz199501@gmail.com>
2025-11-01 10:25:54 +0100

    ajout base de l'application frond end avec menu travailler

---
+++ SAE_5_01frontend2/app/src/main/java/com/example/sae_5_01_frontend/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.sae_5_01_frontend.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
